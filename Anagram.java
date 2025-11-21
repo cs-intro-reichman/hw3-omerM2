454e867a5f1faf0d92b93d5776373d676e0e246a
omer <omermaayan@icloud.com>
2025-11-21 12:17:41 +0200

    update

--- Anagram.java
+++ Anagram.java
@@ -1,4 +1,6 @@
 /** Functions for checking if a given string is an anagram. */
+import java.util.Random;
+
 public class Anagram {
 	public static void main(String args[]) {
 		// Tests the isAnagram function.
@@ -29,21 +31,78 @@ public class Anagram {
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
 		// Replace the following statement with your code
-		return false;
+
+		String s1 = preProcess(str1);
+		String s2 = preProcess(str2);
+
+		if (s1.length() != s2.length()) {
+			return false;
+		}
+
+		char[] arr2 = s2.toCharArray();
+
+		for (int i = 0; i < s1.length(); i++) {
+			char c1 = s1.charAt(i);
+			boolean found = false;
+
+			for (int j = 0; j < arr2.length; j++) {
+				if (arr2[j] == c1) {
+					arr2[j] = 0;  
+					found = true;
+					break;
+				}
+			}
+
+			if (!found) {
+				return false;
+			}
+		}
+
+		return true;
 	}
-	   
+
 	// Returns a preprocessed version of the given string: all the letter characters are converted
 	// to lower-case, and all the other characters are deleted, except for spaces, which are left
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
 		// Replace the following statement with your code
-		return "";
+			String out = "";
+
+		for (int i = 0; i < str.length(); i++) {
+			char c = str.charAt(i);
+
+			
+			if (c >= 'A' && c <= 'Z') {
+				c = (char)(c + ('a' - 'A'));
+				out = out + c;
+			}
+			
+			else if (c >= 'a' && c <= 'z') {
+				out = out + c;
+			}
+			
+		}
+
+		return out;
 	} 
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
 	public static String randomAnagram(String str) {
 		// Replace the following statement with your code
-		return "";
+	Random r = new Random();
+
+		String pool = str;  
+		String out = "";    
+
+		while (pool.length() > 0) {
+			int idx = r.nextInt(pool.length());
+			out = out + pool.charAt(idx);
+
+			
+			pool = pool.substring(0, idx) + pool.substring(idx + 1);
+		}
+
+		return out;
 	}
 }
